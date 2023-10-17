09163579cd0a08d253dd0ab394d37ed8caddf411
dilarakiratli <115098130+dilarakiratli@users.noreply.github.com>
2023-10-17 18:42:21 +0300

    create layout

---
+++ task2_laravels/resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Laravel App</title>
+    <!-- CSS ve JS bağlantıları buraya ekleyebilirsiniz -->
+</head>
+<body>
+    <header>
+        <!-- Başlık, menü veya logo buraya gelebilir -->
+    </header>
+    
+    <main>
+        @yield('content') <!-- Ana içerik buraya gelecek -->
+    </main>
+
+    <footer>
+        <!-- Footer içeriği buraya gelebilir -->
+    </footer>
+</body>
+</html>
